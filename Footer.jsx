a2e1f700e678e7c6bf4cbcd678b3832b30de08b6
NikitaNehra <85426966+NikitaNehra@users.noreply.github.com>
2021-06-06 13:20:06 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+
+  var currYear= new Date().getFullYear();
+  return ( 
+   <footer>
+     <p>copyright @ 2021</p>
+      
+   </footer>  
+   );
+}
+
+export default Footer;
